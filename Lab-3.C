bf53f58e0544a000106f0670b11a845954275a35
EduardRolik <160303012+EduardRolik@users.noreply.github.com>
2024-04-06 12:57:17 +1000

    Update Lab-3.C

--- Lab-3.C
+++ Lab-3.C
@@ -1,18 +1,30 @@
 #include <stdio.h> 
+#include <math.h> 
+ 
+double calculatePerimeter(double a, double b, double c) { 
+    return a + b + c; 
+} 
+ 
+double calculatePloshad(double a, double b, double c) { 
+    double semiPerimeter = calculatePerimeter(a, b, c) / 2; 
+    return sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c)); 
+} 
  
 int main() { 
-    char str[81]; 
+    double a, b, c; 
  
-    printf("Введите строку (макс. 80 символов): "); 
-    scanf("%80s", str); 
+    printf("Введите длины сторон треугольника: "); 
+    scanf("%lf %lf %lf", &a, &b, &c); 
  
-    for (int i = 0; str[i] != '\0'; i++) { 
-        if (str[i] == 'a' || str[i] == 'b') { 
-            str[i] = str[i] - 32; 
-        } 
+    if (a + b > c && a + c > b && b + c > a) { 
+        double perimeter = calculatePerimeter(a, b, c); 
+        double ploshad = calculatePloshad(a, b, c); 
+ 
+        printf("Периметр треугольника: %lf\n", perimeter); 
+        printf("Площадь треугольника: %lf\n", ploshad); 
+    } else { 
+        printf("Треугольник с такими сторонами не существует!\n"); 
     } 
- 
-    printf("Измененная строка: %s\n", str); 
  
     return 0; 
 }
